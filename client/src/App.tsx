5cecb77035c55eae6427b525f35ccf34492152cc
deswaq1220 <deswaq789@naver.com>
2024-03-27 22:52:15 +0900

    chore: 프로젝트 초기세팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
+import { style1 } from './app.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    axios.get('/users').then(res => console.log(res))
+  }, [])
 
   return (
     <>
@@ -16,7 +22,7 @@ function App() {
           {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className={style1}>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ client/src/app.css.ts
@@ -0,0 +1,3 @@
+import { style } from "@vanilla-extract/css";
+
+export const style1 = style({ backgroundColor: "#24ABBE" });
