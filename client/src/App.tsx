b7e709efd42de717b3b916122148dd84a8dacbf4
pparksse1 <pparksse1@gmail.com>
2024-04-04 19:36:32 +0900

    Chore:코드수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,15 @@
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 
-
 import { Outlet, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import BudgetNExpenses from "./Pages/BudgetNExpenses/BudgetNExpenses";
 import BudgetRegister from "./Pages/BudgetRegister/BudgetRegister";
+import Main from "./Pages/MainPage/Main";
 import {
   BUDGET_N_FIXED_EXPENSES_COMPONENT,
   BUDGET_REGISTER_PAGE,
 } from "./constants/components-contants";
-
-
 
 const Layout = () => {
   return (
@@ -27,7 +25,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Main />} />
           <Route
             path={BUDGET_N_FIXED_EXPENSES_COMPONENT}
@@ -36,10 +34,9 @@ function App() {
 
           <Route path={BUDGET_REGISTER_PAGE} element={<BudgetRegister />} />
           <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+          <Route path="/signup" element={<Signup />} />
         </Route>
-      </Routes >
-
+      </Routes>
     </>
   );
 }
