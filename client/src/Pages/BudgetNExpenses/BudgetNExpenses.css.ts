6f526446fc946ef03e8ea489623e5dbd74a4cc38
jhsonnn <jihyeongson13@gmail.com>
2024-04-11 18:40:01 +0900

    Style : 버튼 그림자 등 mui 기본 스타일 제거, 레이아웃 간격 재조정

--- client/src/Pages/BudgetNExpenses/BudgetNExpenses.css.ts
+++ client/src/Pages/BudgetNExpenses/BudgetNExpenses.css.ts
@@ -12,50 +12,67 @@ export const container = style({
   flexDirection: "column",
   backgroundColor: "rgba(255,255,255,0.8)",
   borderRadius: "1rem",
-  padding: 0,
+  padding: "0",
+  width: "900px",
 });
-
+export const wrapper = style({
+  padding: "60px 24px",
+});
 export const totalBudgetBox = style({
-  padding: "60px 32px",
+  width: "100%",
+  height: "40%",
 });
 export const headText = style({
   fontSize: "26px",
   fontWeight: "bold",
+  paddingBottom: "0.3rem",
 });
 export const explainText = style({
-  fontSize: "0.8rem",
+  fontSize: "0.9rem",
   fontWeight: "500",
   display: "flex",
   flexDirection: "column",
   flexGrow: 1,
-  marginRight: "24px",
   color: "#8E8E8E",
-  marginTop: "10px",
-  marginBottom: "5px",
+  paddingBottom: "0.15rem",
 });
 
 //#region 예산 등록
 export const budgetInputBox = style({
   display: "flex",
   alignItems: "center",
-  paddingBottom: "20px",
-  paddingTop: "16px",
+  paddingTop: "1rem",
 });
 export const budgetHistory = style({
-  //marginBottom: "40px",
+  paddingTop: "1rem",
+  marginBottom: "2.5rem",
 });
 export const bugetHistoryTable = style({
   marginLeft: "10px",
   marginTop: "10px",
 });
 export const fixedExpensesRegiTable = style({
-  marginLeft: "10px",
-  marginTop: "16px",
+  paddingTop: "1rem",
+  paddingLeft: "10px",
 });
 //#endregion
 
 //#region 고정 지출 등록
 export const fixedExpenseBox = style({
-  padding: "0px 32px",
+  width: "100%",
+  height: "60%",
 });
 //#endregion
+
+// export const addRowBtn = style({
+//   backgroundColor: "#F03167",
+//   borderRadius: "8px",
+//   marginLeft: "8px",
+//   width: "90px",
+//   height: "38px",
+//   fontSize: "11px",
+//   // hover": {
+//   //   backgroundColor: "#F03167",
+//   // },
+//   boxShadow: "none",
+// });
