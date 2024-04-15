c44406549d5fcd3cf2a83d72cf8ac4a810949952
jhsonnn <jihyeongson13@gmail.com>
2024-04-15 16:16:28 +0900

    Feat : 데이터 fetch & 차트 출력

--- client/src/Components/Charts/DoughnutChartCategory.tsx
+++ client/src/Components/Charts/DoughnutChartCategory.tsx
@@ -1,71 +1,74 @@
 import { ArcElement, Chart, Tooltip } from "chart.js";
+import { useEffect } from "react";
 import { Doughnut } from "react-chartjs-2";
-import testData from "../../pages/MonthlyReport/testDB";
 
 Chart.register(ArcElement, Tooltip);
 
-//#region 타입정의
-
-interface CategoryExpense {
-  category: string;
-  amount: number;
+interface FetchedData {
+  total_expenses_by_category: {
+    id: number;
+    content: string;
+    total_price: number;
+  }[];
+  total_expenses_by_location: {
+    location: string | null;
+    total_price: number;
+  }[];
 }
 
-//#endregion
+interface Top5CategoriesData {
+  content: string;
+}
+const DoughnutChartCategory = ({
+  fetchedData,
+  top5CategoriesData,
+}: {
+  fetchedData: FetchedData;
+  top5CategoriesData: (data: Top5CategoriesData[]) => void;
+}) => {
+  useEffect(() => {
+    const top5Categories = getTop5Categories(fetchedData);
+    top5CategoriesData(top5Categories);
+  }, [fetchedData, top5CategoriesData]);
 
-// testDB의 카테고리 별로 지출금액 합산하는 함수
-const getTotalExpensesPerCategories = (data) => {
-  const totalExpensesPerCategories = {};
-
-  testData.expenses.forEach((expense) => {
-    if (totalExpensesPerCategories[expense.category]) {
-      totalExpensesPerCategories[expense.category] += expense.amount;
-    } else {
-      totalExpensesPerCategories[expense.category] = expense.amount;
+  const getTop5Categories = (fetchedData: FetchedData) => {
+    if (!fetchedData || !fetchedData.total_expenses_by_category) {
+      // null 또는 undefined 경우 처리
+      return [];
     }
-  });
-  return totalExpensesPerCategories;
+
+    //내림차순 정렬
+    const sortedTotalExpensesByCategory =
+      fetchedData.total_expenses_by_category.sort(
+        (a, b) => b.total_price - a.total_price
+      );
+
+    //top5 카테고리 추출
+    const top5Categories = sortedTotalExpensesByCategory.slice(0, 5);
+    //console.log("top5Categories:", top5Categories);
+
+    return top5Categories;
+  };
+
+  const top5Categories = getTop5Categories(fetchedData);
+
+  const data = {
+    datasets: [
+      {
+        label: "지출",
+        data: top5Categories.map((category) => category.total_price),
+        backgroundColor: [
+          "rgba(240,49,103, 0.8)",
+          "rgba(254,172,202,0.8)",
+          "rgba(239,217,158,1)",
+          "rgba(248,161,159,1)",
+          "rgba(251,234,235,1)",
+        ],
+        borderWidth: 10,
+      },
+    ],
+  };
+
+  return <Doughnut data={data} />;
 };
-
-// 카테고리 별 지출 금액을 합산해서 객체 만들기
-const totalExpensesPerCategoriesResult =
-  getTotalExpensesPerCategories(testData);
-
-//#region 지출 가장 큰 곳 top 5 구하기
-
-// totalExpensesPerCategoriesResult 이걸 지출 순서로 내림차순 정렬
-// 1. 객체를 배열로 변환
-const objectToArray = Object.entries(totalExpensesPerCategoriesResult);
-
-//2. 내림차순 정렬
-const sortedArrayResult = objectToArray.sort((a, b) => b[1] - a[1]);
-//console.log("sortedArrayResult : ", sortedArrayResult);
-
-//3. top5 뽑기
-export const top5Categories: CategoryExpense[] = sortedArrayResult.slice(0, 5);
-console.log("top5Categories:", top5Categories);
-
-// //4. 2를 다시 객체로 변환
-// const sortedObjectResult = Object.fromEntries(sortedArrayResult);
-// console.log("sortedObjectResult", sortedObjectResult);
-
-const data = {
-  datasets: [
-    {
-      label: "지출",
-      data: top5Categories.map((category) => category[1]),
-      backgroundColor: [
-        "rgba(240,49,103, 0.8)",
-        "rgba(254,172,202,0.8)",
-        "rgba(239,217,158,1)",
-        "rgba(248,161,159,1)",
-        "rgba(251,234,235,1)",
-      ],
-      borderWidth: 10,
-    },
-  ],
-};
-
-const DoughnutChartCategory = () => <Doughnut data={data} />;
-
 export default DoughnutChartCategory;
--- client/src/Pages/MonthlyReport/MonthlyReport.tsx
+++ client/src/Pages/MonthlyReport/MonthlyReport.tsx
@@ -1,12 +1,11 @@
 import { Box, Divider } from "@mui/material";
 import { Text } from "@radix-ui/themes";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 import BarChart from "../../components/Charts/BarChart";
-import DoughnutChartCategory, {
-  top5Categories,
-} from "../../components/Charts/DoughnutChartCategory";
-import DoughnutChartPlace, {
-  top5Places,
-} from "../../components/Charts/DoughnutChartPlace";
+import DoughnutChartCategory from "../../components/Charts/DoughnutChartCategory";
+import DoughnutChartPlace from "../../components/Charts/DoughnutChartPlace";
 import {
   barChart,
   barChartBox,
@@ -28,10 +27,78 @@ import {
   titleWrapper,
 } from "./MonthlyReport.css";
 
+interface MonthlyReportData {
+  total_expenses_by_category: {
+    id: number;
+    content: string;
+    total_price: number;
+  }[];
+  total_expenses_by_location: {
+    location: string | null;
+    total_price: number;
+  }[];
+}
+
 function MonthlyReport() {
-  // 가져온 데이터 확인
-  //console.log("App.tsx_지출 top5 카테고리 : ", top5Categories);
-  //console.log("App.tsx_지출 top5 장소 : ", top5Places);
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear().toString();
+  const currentMonth = (currentDate.getMonth() + 1).toString();
+
+  const [year, setYear] = useState(currentYear);
+  const [month, setMonth] = useState(currentMonth);
+  const [memberId, setMemberId] = useState("1");
+  // const [accessToken, setAccessToken] = useState("");
+  const [cookies, setCookies] = useCookies(["accessToken", "refreshToken"]);
+  //const [cookies, setCookies] = useCookies(["accessToken"]);
+  const [data, setData] = useState(null);
+
+  //top5Categories state
+  const [top5CategoriesData, setTop5CategoriesData] = useState<
+    MonthlyReportData[]
+  >([]);
+
+  //top5Places state
+  const [top5PlacesData, setTop5PlacesData] = useState<MonthlyReportData[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const config = {
+          headers: {
+            Authorization: `Bearer ${cookies.accessToken}`,
+            // "ngrok-skip-browser-warning": "69420",
+          },
+        };
+
+        const response = await axios.get(
+          `http://ec2-3-35-3-27.ap-northeast-2.compute.amazonaws.com/api/v1/reports/${memberId}?year=${year}&month=${month}`,
+          config
+        );
+
+        setData(response.data);
+        // 새로운 토큰을 쿠키에 저장
+        if (response.data.refreshToken && response.data.accessToken) {
+          setCookies("refreshToken", response.data.refreshToken, {
+            path: "/monthlyreport",
+          });
+          setCookies("accessToken", response.data.accessToken, {
+            path: "/monthlyreport",
+          });
+        }
+
+        console.log(response);
+
+        if (response.data) {
+          console.log("Data : ", response.data);
+        } else {
+          console.log("Empty response data");
+        }
+      } catch (error) {
+        console.error("Error fetching monthly report:", error);
+      }
+    };
+    fetchData();
+  }, [memberId, cookies.accessToken, year, month, setCookies]);
 
   return (
     <Box className={box}>
@@ -59,9 +126,10 @@ function MonthlyReport() {
               </Box>
               <Box className={ranking}>
                 <ul className={list}>
-                  {top5Categories.map((category, index) => (
+                  {top5CategoriesData.map((category, index) => (
                     <li className={li} key={index + 1}>
-                      {index + 1}. {category[0]}
+                      {index + 1}.
+                      {category.content !== null ? category.content : "기타"}
                     </li>
                   ))}
                 </ul>
@@ -69,7 +137,10 @@ function MonthlyReport() {
             </Box>
             <Box className={doughnutChartBox}>
               <Box className={doughnutChart}>
-                <DoughnutChartCategory />
+                <DoughnutChartCategory
+                  fetchedData={data}
+                  top5CategoriesData={setTop5CategoriesData}
+                />
               </Box>
             </Box>
           </Box>
@@ -78,9 +149,10 @@ function MonthlyReport() {
               <Box className={explainTitle}>이번 달 가장 큰 지출이에요</Box>
               <Box className={ranking}>
                 <ul className={list}>
-                  {top5Places.map((place, index) => (
-                    <li className={li} key={index}>
-                      {index + 1}. {place[0]}
+                  {top5PlacesData.map((place, index) => (
+                    <li className={li} key={index + 1}>
+                      {index + 1}.{" "}
+                      {place.location !== null ? place.location : "기타"}
                     </li>
                   ))}
                 </ul>
@@ -88,14 +160,17 @@ function MonthlyReport() {
             </Box>
             <Box className={doughnutChartBox}>
               <Box className={doughnutChart}>
-                <DoughnutChartPlace />
+                <DoughnutChartPlace
+                  fetchedData={data}
+                  top5PlacesData={setTop5PlacesData}
+                />
               </Box>
             </Box>
           </Box>
         </Box>
         <Box className={barChartBox}>
           <Box className={barChart}>
-            <BarChart />
+            <BarChart fetchedData={data} />
           </Box>
         </Box>
       </Box>
