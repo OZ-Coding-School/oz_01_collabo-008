52b531cce78099b919d3d37dbeb68e5b41add042
SwanyCastle <seunghwan712@gmail.com>
2024-04-20 23:01:23 +0900

    Deploy: nginx, gunicorn 설정 및 docker-compose, dockerfile 수정

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# python manage.py collectstatic
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+# echo "Apply database migrations"
 # python manage.py makemigrations
 # python manage.py migrate
-python manage.py runserver 0.0.0.0:80
+
+# Start Gunicorn
+echo "Starting Gunicorn"
+gunicorn myproject.wsgi:application --bind 0.0.0.0:8000
